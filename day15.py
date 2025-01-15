3dc2e5074cb7d84c0664cca76093fc48d433d3a1
N.G <sixthnovember@protonmail.com>
2025-01-15 06:40:37 +0100

    Create day15.py

---
+++ day15.py
@@ -0,0 +1,11 @@
+# Leetle Day 15 - Matrix Rotation
+# 15.01.2025
+
+def solve(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for row in matrix:
+        row.reverse()
+    return matrix
