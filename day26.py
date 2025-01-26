2721a43759e6081bef093378970db572941f0509
N.G <sixthnovember@protonmail.com>
2025-01-26 06:47:19 +0100

    Create day26.py

---
+++ day26.py
@@ -0,0 +1,12 @@
+# Leetle Day 26 - Merge Intervals
+# 26.01.2025
+
+def solve(intervals):
+    intervals.sort()
+    merged = []
+    for start, end in intervals:
+        if not merged or start > merged[-1][1]:
+            merged.append([start, end])
+        else:
+            merged[-1][1] = max(merged[-1][1], end)
+    return merged
