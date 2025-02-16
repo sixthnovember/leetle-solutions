db1ee04bfbea328751f3511057e4f1477e04c920
N.G <sixthnovember@protonmail.com>
2025-02-16 06:09:36 +0100

    Create day47.py

---
+++ 2025/02/day47.py
@@ -0,0 +1,12 @@
+# Leetle Day 47 - Integer to Roman
+# 16.02.2025
+
+def solve(num):
+    roman_numerals = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+                      (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+                      (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+    result = []
+    for value, symbol in roman_numerals:
+        result.append(symbol * (num // value))
+        num %= value
+    return "".join(result)
